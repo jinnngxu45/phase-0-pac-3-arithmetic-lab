f21fc756e109ed00a5bd37bb3d7cc7c9cc5b4e52
“Jing <+>
2021-11-17 22:15:18 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b){
+    return a + b;
+}
+console.log (add(4,2));
+
+function subtract(a,b){
+    return a - b;
+}
+console.log (add(4,2));
+
+function multiply(a,b){
+    return a * b;
+}
+console.log (add(4,2));
+
+function divide(a,b){
+    return a / b;
+}
+console.log (add(4,2));
+
+number = 10;
+
+function increment(n) {
+    n++
+  return n;
+}
+
+function decrement(n) {
+    n--
+  return n;
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n, 10);
+}
